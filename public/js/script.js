6183b13fcf86377af92da21fd7fbf38259e69232
edoalvin <edoalvin123@gmail.com>
2025-05-01 18:07:21 +0700

    feat: header & hero

---
+++ public/js/script.js
@@ -0,0 +1,5 @@
+const gerry= document.createElement("h3");
+const akbar= document.createTextNode("shit");
+gerry.appendChild(akbar)
+const rifki= document.querySelector("#edo")
+rifki.appendChild(gerry)
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "3366FF",
+        secondary: "F8D57E",
+      }
+    },
+  },
+  plugins: [],
+}
