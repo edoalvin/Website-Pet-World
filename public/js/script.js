510e6cbcd0501b6d65584def9378c024b0077733
edoalvin <edoalvin123@gmail.com>
2025-06-23 19:39:05 +0700

    feat: footer & animasi gambar

--- public/js/script.js
+++
@@ -1,5 +0,0 @@
-const gerry= document.createElement("h3");
-const akbar= document.createTextNode("shit");
-gerry.appendChild(akbar)
-const rifki= document.querySelector("#edo")
-rifki.appendChild(gerry)
